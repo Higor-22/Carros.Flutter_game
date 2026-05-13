afa4e88e7922b36b026469cfe35b943179bcf227
Higor S. Galvão <higorsantanagalvao@gmail.com>
2026-05-13 11:44:41 -0300

    Atualização do Jogo

--- carros/lib/main.dart
+++ carros/lib/main.dart
@@ -16,3 +16,4 @@ class CarGameApp extends StatelessWidget {
     );
   }
 }
+// This is a basic Flutter application that serves as the entry point for a 2D car game. It defines a `CarGameApp` widget that sets up the MaterialApp with a dark theme and specifies the `MenuScreen` as the home screen. The debug banner is also disabled for a cleaner look.
